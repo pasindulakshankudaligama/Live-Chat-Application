f34a448925a9a92384c0f15812b0b382d3a87316
pasindulakshankudaligama <kudaligamagepasindulakshan@gmail.com>
2022-08-16 01:42:24 +0530

    client collection added

---
+++ src/model/ClientConnection.java
@@ -0,0 +1,60 @@
+package model;
+
+import controller.ServerFormController;
+
+import java.io.*;
+import java.net.Socket;
+
+
+public class ClientConnection implements Runnable {
+    private final Socket accept;
+    private final ServerFormController server;
+    private BufferedReader bufferedReader;
+    private BufferedWriter bufferedWriter;
+//    private PrintWriter printWriter;
+
+    public ClientConnection(Socket socket, ServerFormController server) {
+        this.accept = socket;
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(accept.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            while (true) {
+                String readLine = bufferedReader.readLine();
+                System.out.println("Line : " + readLine);
+                server.broadcast(readLine);
+                server.txtMsgDisplay.appendText("client : " + readLine + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                accept.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        }
+
+    }
+
+    //send message back to client
+    public void sendMessage(String message) {
+        try {
+            PrintWriter printWriter = new PrintWriter(accept.getOutputStream());
+            printWriter.println(message);
+            System.out.println("He hee : "+message);
+            printWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
